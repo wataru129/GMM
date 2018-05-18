e4d565d12618a3272972fc15a4e7e096724d2117
wataru <you@example.com>
2018-05-18 11:56:59 +0900

    nice

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import time
 from src.main_function   import *
 
+start = time.time()
 # パラメータ設定ファイルの指定
 parameter_file = './setting/parameter.yaml'
 # 設定パラメータ(yaml)の読み込み
@@ -11,7 +13,6 @@ params = load_parameters(parameter_file)
 dataset = eval(params['general']['development_dataset'])(data_path=params['path']['data'])
 params['features']['mfcc']['win_length'] = int(params['features']['win_length_seconds'] * params['features']['fs'])
 params['features']['mfcc']['hop_length'] = int(params['features']['hop_length_seconds'] * params['features']['fs'])
-a = dict(params['classifier_parameters']['gmm'])
 files = []
 dataset_evaluation_mode = 'folds'
 for fold in dataset.folds(mode=dataset_evaluation_mode):
@@ -24,8 +25,8 @@ for fold in dataset.folds(mode=dataset_evaluation_mode):
 files = sorted(files)
 # ファイルを調べ、すべての特徴量が抽出されていることを確認する
 print("feature_extract")
-if not os.path.exists("features"):
-    do_feature_extraction(files=files,
+#if not os.path.exists("features"):
+do_feature_extraction(files=files,
                           dataset=dataset,
                           feature_path=params['path']['features'],
                           params=params['features'],
@@ -50,3 +51,39 @@ do_system_training(dataset=dataset,
                            clean_audio_errors=params['classifier']['audio_error_handling']['clean_data'],
                            overwrite=params['general']['overwrite']
                            )
+
+print("create challenge dataset")
+challenge_dataset = eval(params['general']['challenge_dataset'])(data_path=params['path']['data'])
+result_path = params['path']['results']
+if not params['general']['challenge_submission_mode']:
+        # Extract feature if not running in challenge submission mode.
+        # Collect test files
+        files = []
+        for fold in challenge_dataset.folds(mode=dataset_evaluation_mode):
+            for item_id, item in enumerate(dataset.test(fold)):
+                if item['file'] not in files:
+                    files.append(item['file'])
+        files = sorted(files)
+            # Go through files and make sure all features are extracted
+        do_feature_extraction(files=files,
+                                  dataset=challenge_dataset,
+                                  feature_path=params['path']['features'],
+                                  params=params['features'],
+                                  overwrite=params['general']['overwrite'])
+print("testing")
+do_system_testing(dataset=challenge_dataset,
+                  feature_path=params['path']['features'],
+                  result_path=result_path,
+                  model_path=params['path']['models'],
+                  feature_params=params['features'],
+                  dataset_evaluation_mode=dataset_evaluation_mode,
+                  classifier_method=params['classifier']['method'],
+                  clean_audio_errors=params['recognizer']['audio_error_handling']['clean_data'],
+                  overwrite=params['general']['overwrite'] or params['general']['challenge_submission_mode']
+                  )
+print(evaluation)
+do_system_evaluation(dataset=challenge_dataset,
+                     dataset_evaluation_mode=dataset_evaluation_mode,
+                     result_path=result_path)
+elapsed_time = time.time() - start
+print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
