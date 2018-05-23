d4a044a325b031fdc99be3fcfab5baeab85f3cbe
Wataru Hashimoto <hashimoto@atf.co.jp>
2018-05-23 18:05:12 +0900

    no message

--- create_label.py
+++ create_label.py
@@ -1,33 +1,35 @@
 import os
 import pandas as pd
 
-path = "/Users/wataru/Laboratry/reserch/test_extract/dataset/TUT-acoustic-scenes-2016-development/audio"
+#path = "/Users/wataru/Laboratry/reserch/test_extract/dataset/TUT-acoustic-scenes-2016-development/audio"
+path = "/Users/wataru/Laboratry/reserch/test_extract/dataset/data_car"
 
-files = os.listdir(path)
-files_file = [f for f in files if os.path.isfile(os.path.join(path, f))]
+#files = os.listdir(path)
+#files_file = [f for f in files if os.path.isfile(os.path.join(path, f))]
 #print(files_file)   # ['file1', 'file2.txt', 'file3.jpg']
 
+files = []
+files = os.listdir(path)
 
-index = ["a"]
+print(files)
+df = pd.Series(files)
+
+
+#index = ["a"]
 #df = pd.read_csv('some.csv')
-df = pd.Series(files_file[0], index= index)
-print(df)
-
-
-
-df1 = pd.read_table("/Users/wataru/Laboratry/reserch/test_extract/dataset/TUT-acoustic-scenes-2016-development/evaluation_setup/fold1_train.txt", delim_whitespace=True, header=None)
-df2 = pd.read_table("/Users/wataru/Laboratry/reserch/test_extract/dataset/TUT-acoustic-scenes-2016-development/evaluation_setup/fold2_train.txt", delim_whitespace=True, header=None)
-df3 = pd.read_table("/Users/wataru/Laboratry/reserch/test_extract/dataset/TUT-acoustic-scenes-2016-development/evaluation_setup/fold3_train.txt", delim_whitespace=True, header=None)
-df4 = pd.read_table("/Users/wataru/Laboratry/reserch/test_extract/dataset/TUT-acoustic-scenes-2016-development/evaluation_setup/fold4_train.txt", delim_whitespace=True, header=None)
+#df = pd.Series(files_file, index= index)
+#print(df)
+#df  = pd.read_table("/Users/wataru/Laboratry/reserch/test_extract/dataset/TUT-acoustic-scenes-2016-development/evaluation_setup/fold1_train.txt", delim_whitespace=True, header=None)
+#df1 = pd.read_table("/Users/wataru/Laboratry/reserch/test_extract/dataset/TUT-acoustic-scenes-2016-development/evaluation_setup/fold1_train.txt", delim_whitespace=True, header=None)
+#df2 = pd.read_table("/Users/wataru/Laboratry/reserch/test_extract/dataset/TUT-acoustic-scenes-2016-development/evaluation_setup/fold2_train.txt", delim_whitespace=True, header=None)
+#df3 = pd.read_table("/Users/wataru/Laboratry/reserch/test_extract/dataset/TUT-acoustic-scenes-2016-development/evaluation_setup/fold3_train.txt", delim_whitespace=True, header=None)
+#df4 = pd.read_table("/Users/wataru/Laboratry/reserch/test_extract/dataset/TUT-acoustic-scenes-2016-development/evaluation_setup/fold4_train.txt", delim_whitespace=True, header=None)
 #print(df1)
 #print(df2)
 #print(df3)
 #print(df4)
 
-df = pd.concat([df1, df2, df3, df4])
+#df = pd.concat([df1, df2, df3, df4])
 df = df.reset_index(drop=True)
 df.to_csv('train.csv')
 df
-
-a = ["a","b"]
-a
--- src/my_dataset.py
+++ src/my_dataset.py
@@ -7,8 +7,6 @@ class Dataset(object):
     def __init__(self, data_path='data', name='dataset'):
         self.name = name
         self.local_path = os.path.join(data_path, self.name)
-        if not os.path.isdir(self.local_path):
-            os.makedirs(self.local_path)
         self.evaluation_setup_folder = 'evaluation_setup'
         self.evaluation_setup_path = os.path.join(self.local_path, self.evaluation_setup_folder)
         self.filelisthash_filename = 'filelist.python.hash'
@@ -16,76 +14,29 @@ class Dataset(object):
         self.files = None
         self.evaluation_data_train = {}
         self.evaluation_data_test = {}
-        self.audio_extensions = {'wav'}
     def test(self, fold=0):
-        if fold not in self.evaluation_data_test:
-            self.evaluation_data_test[fold] = []
-            if fold > 0:
-                with open(os.path.join(self.evaluation_setup_path, 'fold' + str(fold) + '_test.txt'), 'rt') as f:
-                    for row in csv.reader(f, delimiter='\t'):
-                        self.evaluation_data_test[fold].append({'file': self.relative_to_absolute_path(row[0])})
-            else:
-                data = []
-                files = []
-                for item in self.meta:
-                    if self.relative_to_absolute_path(item['file']) not in files:
-                        data.append({'file': self.relative_to_absolute_path(item['file'])})
-                        files.append(self.relative_to_absolute_path(item['file']))
-                self.evaluation_data_test[fold] = data
+        with open(os.path.join(self.evaluation_setup_path, 'fold' + str(fold) + '_test.txt'), 'rt') as f:
+            for row in csv.reader(f, delimiter='\t'):
+                self.evaluation_data_test[fold].append({'file': self.relative_to_absolute_path(row[0])})
         return self.evaluation_data_test[fold]
     def folds(self, mode='folds'):
         if mode == 'folds':
             return range(1, self.evaluation_folds + 1)
         elif mode == 'full':
             return [0]
-
     def relative_to_absolute_path(self, path):
         return os.path.abspath(os.path.join(self.local_path, path))
     def train(self, fold=0):
         if fold not in self.evaluation_data_train:
             self.evaluation_data_train[fold] = []
-            if fold > 0:
-                print(os.path.join(self.evaluation_setup_path, 'fold' + str(fold) + '_train.txt'))
-                with open(os.path.join(self.evaluation_setup_path, 'fold' + str(fold) + '_train.txt'), 'rt') as f:
-                    for row in csv.reader(f, delimiter='\t'):
-                        if len(row) == 2:
-                            # Scene meta
-                            self.evaluation_data_train[fold].append({
-                                'file': self.relative_to_absolute_path(row[0]),
-                                'scene_label': row[1]
-                            })
-                        elif len(row) == 4:
-                            # Audio tagging meta
-                            self.evaluation_data_train[fold].append({
-                                'file': self.relative_to_absolute_path(row[0]),
-                                'scene_label': row[1],
-                                'tag_string': row[2],
-                                'tags': row[3].split(';')
-                            })
-                        elif len(row) == 5:
-                            # Event meta
-                            self.evaluation_data_train[fold].append({
-                                'file': self.relative_to_absolute_path(row[0]),
-                                'scene_label': row[1],
-                                'event_onset': float(row[2]),
-                                'event_offset': float(row[3]),
-                                'event_label': row[4]
-                            })
-            else:
-                data = []
-                for item in self.meta:
-                    if 'event_label' in item:
-                        data.append({'file': self.relative_to_absolute_path(item['file']),
-                                     'scene_label': item['scene_label'],
-                                     'event_onset': item['event_onset'],
-                                     'event_offset': item['event_offset'],
-                                     'event_label': item['event_label'],
-                                     })
-                    else:
-                        data.append({'file': self.relative_to_absolute_path(item['file']),
-                                     'scene_label': item['scene_label']
-                                     })
-                self.evaluation_data_train[0] = data
+            print(os.path.join(self.evaluation_setup_path, 'fold' + str(fold) + '_train.txt'))
+            with open(os.path.join(self.evaluation_setup_path, 'fold' + str(fold) + '_train.txt'), 'rt') as f:
+                for row in csv.reader(f, delimiter='\t'):
+                    # Scene meta
+                    self.evaluation_data_train[fold].append({
+                        'file': self.relative_to_absolute_path(row[0]),
+                        'scene_label': row[1]
+                    })
         return self.evaluation_data_train[fold]
 class DevelopmentSet(Dataset):
     def __init__(self, data_path='data'):
@@ -93,7 +44,6 @@ class DevelopmentSet(Dataset):
         self.evaluation_folds = 4
     def on_after_extract(self):
         if not os.path.isfile(self.meta_file):
-            section_header('Generating meta file for dataset')
             meta_data = {}
             for fold in xrange(1, self.evaluation_folds):
                 # Read train files in
@@ -154,18 +104,7 @@ class EvaluationSet(Dataset):
     def test(self, fold=0):
         if fold not in self.evaluation_data_test:
             self.evaluation_data_test[fold] = []
-            if fold > 0:
-                with open(os.path.join(self.evaluation_setup_path, 'fold' + str(fold) + '_test.txt'), 'rt') as f:
-                    for row in csv.reader(f, delimiter='\t'):
-                        self.evaluation_data_test[fold].append({'file': self.relative_to_absolute_path(row[0])})
-            else:
-                data = []
-                files = []
-                for item in self.audio_files:
-                    if self.relative_to_absolute_path(item) not in files:
-                        data.append({'file': self.relative_to_absolute_path(item)})
-                        files.append(self.relative_to_absolute_path(item))
-
-                self.evaluation_data_test[fold] = data
-
+            with open(os.path.join(self.evaluation_setup_path, 'fold' + str(fold) + '_test.txt'), 'rt') as f:
+                for row in csv.reader(f, delimiter='\t'):
+                    self.evaluation_data_test[fold].append({'file': self.relative_to_absolute_path(row[0])})
         return self.evaluation_data_test[fold]
